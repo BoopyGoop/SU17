01efa0700edb359e80b3abc1efb4ef54f49e52bf
Carter Carlos <pancakeslayer@hotmail.com>
2017-06-15 15:28:09 -0700

    cleaned up more, more efficient

--- PLA - prob1.4.py
+++ PLA - prob1.4.py
@@ -18,7 +18,6 @@ learningRate = .1
 
 
 class line(object):
-    
     def __init__(self, slope, yintercept):
         self.slope = slope
         self.yintercept = yintercept
@@ -27,14 +26,11 @@ class line(object):
         return xval*self.slope + self.yintercept
 
 
-
 def genPoint(line):
-    
     label = -1
     if (np.random.random() < 0.5):
         label = 1
-    
-    
+      
     x = np.random.random()*20
     if (np.random.random() < 0.5):
         x = -x
@@ -53,28 +49,15 @@ def genPoint(line):
             if (np.random.random() < 0.5):
                 y = -y
             
-    return [label, x, y]
-
-
+    return [label, x, y, 1]
 
 
 def predict(x, pweights):
-    #find activation val with dot product
-    
-    xvec = np.matrix(x[1:])
-    weightsVec = np.matrix(pweights)
-    
-    activation = np.dot(xvec, weightsVec.transpose())
-    
-    if activation >= 0:
-        return 1
-    
-    return -1 
-
+    #find activation val with dot product   
+    return np.dot(x[1:], pweights.transpose())
 
 
 def train(data, lRate):
-    
     weights = np.zeros((len(data[0])-1), dtype = float)
     hasErrors = True
     iterNum = 0
@@ -86,21 +69,13 @@ def train(data, lRate):
         hasErrors = False
         errorCount = 0
         
-        for x in data:
-            #predict classification of point based on weights, compare to actual classification
-            prediction = predict(x, weights)
-            actual = x[0]   
-            error = actual * prediction
-            
-            
-            if (error < 0): #if point is misclassified...
+        for x in points:
+            if (x[0]*predict(x, weights) <= 0): #if point is misclassified...
                 #update weights
-                weights = weights + lRate*actual*x[1:]
+                weights = weights + lRate*x[0]*x[1:]
             
-                
                 hasErrors = True
                 errorCount = errorCount +1
-                
                 
         print("Iteration ", iterNum, "| #errors ", errorCount )
  
@@ -108,21 +83,11 @@ def train(data, lRate):
 
 
 def PLA(points):
-    dimension = len(points[0])
-    
-    #constructs data matrix with labels(last column) from points provided
-    data = np.zeros((pointAmount, dimension+1), dtype = float) 
-    for i in range(pointAmount):
-        data[i, :dimension] = points[i]
-        data[i, dimension] = 1
-        
-    
     #train the weights
-    weights = train(data, learningRate)
+    weights = train(points, learningRate)
     
     print("Weights: ", weights)
     return weights
-
 
 
 #randomly generates target function        
@@ -134,9 +99,8 @@ if (np.random.random() < 0.5):
     yint = -yint
 targFunc = line(slope, yint)
 
-
 #generates equal number of +1 and -1 classified points
-points = []
+points = np.zeros(shape=(pointAmount, len(genPoint(targFunc))))
 for i in range(0, pointAmount):
     point = genPoint(targFunc)
     
@@ -145,12 +109,10 @@ for i in range(0, pointAmount):
     else:
         color = 'red'
               
-        
-    #TODO this could be better generalized?
+    #TODO this could be generalized to handle more than 2D points?
     plt.scatter(point[1], point[2], c=color)
-    points.append(point)
+    points[i] = point
   
-    
 #plot target function
 x = np.arange(-20,20,0.1)
 y = targFunc.findyval(x)
@@ -158,10 +120,8 @@ plt.plot(x,y,c='black', label = "Target Function")
 plt.xlabel('x axis')
 plt.ylabel('y axis')
 
-
 #use points to generate weights
 learnedWeights = PLA(points)
-
 
 #plot learned function
 gSlope = -(learnedWeights[0]/learnedWeights[1])
@@ -172,5 +132,3 @@ y = g.findyval(x)
 plt.plot(x,y,c='green', label = "Learned Function")
 
 plt.legend(loc='upper left')
-
-
