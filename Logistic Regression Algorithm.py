92f7bcde5f5d383b9b74359ef7ab341101a84b13
Carter Carlos <pancakeslayer@hotmail.com>
2017-06-16 14:22:38 -0700

    barebones skellyton code

--- Logistic Regression Algorithm.py
+++ Logistic Regression Algorithm.py
@@ -8,3 +8,39 @@ Created on Fri Jun 16 10:54:37 2017
 Implementation of Logistic Regression Algorithm to distinguish written numbers 3 and 5
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+testFile = "zip.test"
+trainFile = "zip.train"
+
+#TODO modify these params to be dynamically decided
+iterations = 50
+learningRate = 0.1
+
+
+def computeGradient(data, weights):
+    
+    
+def updateWeights(direction, weights, lRate):
+    
+
+def logRegressAlgo(data):
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+#import training data
+data = np.loadtxt(trainFile)
+print(data)
+print("Cols:", len(data[0]))
+print("Rows:", len(data))
