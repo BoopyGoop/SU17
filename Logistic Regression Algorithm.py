594a7ebe10be44980c6ffd5b18c9b8cd7fc7d756
Carter Carlos <pancakeslayer@hotmail.com>
2017-06-16 11:34:00 -0700

    implementing logistic regrssion

---
+++ Logistic Regression Algorithm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 10:54:37 2017
+
+@author: Carter Carlos
+
+
+Implementation of Logistic Regression Algorithm to distinguish written numbers 3 and 5
+"""
+
