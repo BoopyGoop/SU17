f51122a1183407bb08909f547db9ec47b8e9120a
Carter Carlos <pancakeslayer@hotmail.com>
2017-07-05 12:13:01 -0700

    two conv layers, ~94% accurate

--- Convolutional NN.py
+++ Convolutional NN.py
@@ -10,60 +10,98 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('/mnist',one_hot=True)
 
-#number of nodes in the hidden layer
-numNodesHL = 100
 
-iterations = 10
+#ARCHITECTURE:
+#INPUT -> CONV -> RELU -> POOL -> CONV -> RELU -> POOL -> FC -> OUTPUT
 
+iterations = 2000
 
 numClasses = 10
 inputSize = 784
 batchSize = 10
 
+nodesFClayer = 500
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def maxPool2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+
+def deepNN(x):
+    xReshape = tf.reshape(x, [-1, 28, 28, 1])
+    
+    #Conv layer 1
+    wConv = weight_variable([5, 5, 1, 32])
+    bConv = bias_variable([32])
+    hConv = tf.nn.relu(conv2d(xReshape, wConv) + bConv)
+    
+    #Pool layer 1
+    hPool = maxPool2x2(hConv)
+    
+    
+    #Conv layer 2
+    wConv2 = weight_variable([5, 5, 32, 64])
+    bConv2 = bias_variable([64])
+    hConv2 = tf.nn.relu(conv2d(hPool, wConv2) + bConv2)
+    
+    #Pool layer 2
+    hPool2 = maxPool2x2(hConv2)
+    
+    
+    #Fully connected layer
+    wFC = weight_variable([7*7*64, nodesFClayer])
+    bFC = bias_variable([nodesFClayer])
+    hPool2Flat = tf.reshape(hPool2, [-1, 7*7*64])
+    hFC = tf.nn.relu(tf.matmul(hPool2Flat, wFC) + bFC)
+    
+    
+    #Output layer
+    wOut = weight_variable([nodesFClayer, numClasses])
+    bOut = bias_variable([numClasses])
+    
+    yConv = tf.matmul(hFC, wOut) + bOut
+
+    return yConv
+
+
 
 x = tf.placeholder('float', [None, inputSize])
-y = tf.placeholder('float')
+y = tf.placeholder('float', [None, numClasses])
 
-def NN_model(data):
-    
-    #dictionaries hold wiehgts/biases of each layer, randomly generated at start
-    hiddenLayer = {'weights': tf.Variable(tf.random_normal([inputSize, numNodesHL])), 'biases': tf.Variable(tf.random_normal([numNodesHL]))}
-    outputLayer = {'weights': tf.Variable(tf.random_normal([numNodesHL, numClasses])), 'biases': tf.Variable(tf.random_normal([numClasses]))}
-    
-    hLayer = tf.add(tf.matmul(data, hiddenLayer['weights']), hiddenLayer['biases'])
-    hLayer = tf.nn.relu(hLayer)
-    output = tf.add(tf.matmul(hLayer, outputLayer['weights']), outputLayer['biases'])
-    
-    return output
+yConv = deepNN(x)
 
-    
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=yConv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(yConv, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-def train(x, iterations):
-    prediction = NN_model(x)
-
-    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = prediction, labels = y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    for i in range(iterations):
+        batch = mnist.train.next_batch(batchSize)
         
-        for iteration in range(iterations):
-            iterationLoss = 0
+        if i %100 == 0:
             
-            for i in range(int(mnist.train.num_examples/batchSize)):
-                xIter, yIter = mnist.train.next_batch(batchSize)
-                i, c = sess.run([optimizer, cost], feed_dict = {x: xIter, y: yIter})
-                iterationLoss = iterationLoss + c
+            trainAcc = accuracy.eval(feed_dict={x: batch[0], y: batch[1]})
+            print("Step", i, ", training accuracy", trainAcc)
+            
+        train_step.run(feed_dict= {x: batch[0], y: batch[1]})
+        
+    print("")
+    print("test accuracy", accuracy.eval(feed_dict= { x: mnist.validation.images, y: mnist.validation.labels}))
 
-            print("Iteration", iteration, "of", iterations-1, "completed")
-            print("Loss:", iterationLoss)
-            print("")
-
-        correct = tf.equal(tf.arg_max(prediction, 1), tf.arg_max(y,1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print("Accuracy:", accuracy.eval({x: mnist.validation.images, y: mnist.validation.labels}))
-
-
-train(x, iterations)
 
